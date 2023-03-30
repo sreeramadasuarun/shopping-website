9f02c30f9e7ff8c8f340c228fade11bc0ac9003c
sreeramadasuarun <sreeramadasuarun@gmail.com>
2023-03-30 22:04:15 +0530

    succesful of search

---
+++ src/Componets/shop.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import shopdata from "./shopdata";
+import "../Componets/shopstyles.css";
+
+const Shop = () => {
+  const [names, setnames] = useState(shopdata);
+  const [filternames, setfilternames] = useState("");
+
+  console.log(names);
+
+  console.log(filternames);
+
+  const result = (e) => {
+    e.preventDefault();
+    const filterednames = names.filter((names) =>
+      names.title.toLowerCase().includes(filternames)
+    );
+    setnames(filterednames);
+  };
+
+  return (
+    <div className="allcard">
+      <form>
+        <label>name: </label>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          value={filternames}
+          onChange={(e) => setfilternames(e.target.value)}
+          placeholder="search name"
+        />
+        <button onClick={result} type="submit">
+          click
+        </button>
+      </form>
+      <h1>shopping</h1>
+      <div className="cardcon">
+        {names.map(({ title, price, category, id, image }) => {
+          return (
+            <div key={id}>
+              <article className="card">
+                <div className="image">
+                  <img src={image} alt="noimage" />
+                </div>
+                <h3>{title.split(" ").slice(0, 2)}</h3>
+
+                <div className="right-left">
+                  <div className="left">
+                    <span>price:</span>
+
+                    <span className="bold">{price}</span>
+                  </div>
+                  <div className="left">
+                    <span>category:</span>
+
+                    <span className="bold">{category}</span>
+                  </div>
+                </div>
+              </article>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
